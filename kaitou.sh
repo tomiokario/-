627b3d1fcd1e2176cdb159dc433f7176c77f5623
TomiokaRio <riopf239@gmail.com>
2017-09-12 22:49:51 +0900

    間違えた場合のための戻る機能の追加

--- kaitou.sh
+++ kaitou.sh
@@ -1,22 +1,28 @@
 #◯×問題解の解答を作成するプログラム
 #!/bin/bash
 ad=`pwd`;cd;cd Desktop
-count=1
-read -p "作成するファイル名" file
+
 echo "解答ファイルを作成します"
+read -p "ファイル名を入力:" file
 echo "解答を入力"
-echo "解答" > "seikai"$file
-echo "[ ○)1, ×)2 ]終わったらqを入力"
+list2[0]="解答"
+count=1
+echo "[ ○)1, ×)2, 戻る)b, 終了)q ]"
 while :
 do
     read -p $count" :" res
-    until [ $res = "1" -o $res = "2" -o $res = "q" ]; do
+    until [ $res = "1" -o $res = "2" -o $res = "q" -o $res = "b" ]; do
         read -p $count" :" res
     done
     case $res in
-    1) count=`expr $count + 1`;echo ○ >> "seikai"$file;;
-    2) echo × >> "seikai"$file;count=`expr $count + 1`;;
-    q) echo 終了;count=1;break;;
+        1) list2[$count]="○";count=`expr $count + 1`;;
+        2) list2[$count]="×";count=`expr $count + 1`;;
+        b) count=`expr $count - 1`;;
+        q) echo 終了;lnum=$count;break;;
     esac
+done
+echo ${list2[0]} > "seikai"$file
+for i in `seq $lnum` ;do
+    echo ${list2[i]} >> "seikai"$file
 done
 cd;cd $ad
